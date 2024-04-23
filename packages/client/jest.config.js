2ec36f9498190a17668e04e90a8bd17cc29ed1c6
AndreyShulga <podkudahka@gmail.com>
2024-04-23 02:30:58 +0200

    Check precomit

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -5,6 +5,9 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  moduleNameMapper: {
+    '\\.(css|scss)$': '<rootDir>/jest/__mocks__/styleMock.js',
+  },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
