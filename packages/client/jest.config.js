a59edc783a2da081bad523425169c2c57c59e5ed
AndreyShulga <podkudahka@gmail.com>
2024-04-24 01:54:43 +0200

    refactoring

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -5,9 +5,6 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
-  moduleNameMapper: {
-    '\\.(css|scss)$': '<rootDir>/jest/__mocks__/styleMock.js',
-  },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
