9c88f84fc014093ae24cb49d696e57240f00a844
AndreyShulga <podkudahka@gmail.com>
2024-04-24 18:07:18 +0200

    refactoring

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,17 +1,17 @@
 // TODO: Need to fix scss parsing in Jest tests
 // https://stackoverflow.com/questions/54627028/jest-unexpected-token-when-importing-css
 
-// import App from './App'
-// import { render, screen } from '@testing-library/react'
-//
-// const appContent = 'Вот тут будет жить ваше приложение :)'
+import App from './App'
+import { render, screen } from '@testing-library/react'
+
+const titleText = 'Bugs Survivors'
 
 // @ts-ignore
-// global.fetch = jest.fn(() =>
-//   Promise.resolve({ json: () => Promise.resolve('hey') })
-// )
+global.fetch = jest.fn(() =>
+  Promise.resolve({ json: () => Promise.resolve('hey') })
+)
 
 test('Example test', async () => {
-  // render(<App />)
-  expect('1').toBe('1')
+  render(<App />)
+  expect(screen.getByText(titleText)).toBeDefined()
 })
--- packages/client/src/pages/Discovery/index.tsx
+++ packages/client/src/pages/Discovery/index.tsx
@@ -8,7 +8,7 @@ const Discovery = () => {
       <img className={styles.background} src="/bgImage.png" alt="" />
       <div className={clsx('container', styles.container)}>
         <div className={styles.wrapper}>
-          <h1 className={styles.title}>Bugs Survivors</h1>
+          <h1 className={styles.title}>Bugs Survivors2</h1>
           <h4 className={styles.title}>By StathamGames</h4>
           <Button variant="contained">Play game</Button>
           <ul className={styles.links}>
