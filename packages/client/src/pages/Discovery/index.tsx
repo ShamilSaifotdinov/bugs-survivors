58998d285f28a76a7adf1eac9592aed13da979dc
AndreyShulga <podkudahka@gmail.com>
2024-04-24 19:55:16 +0200

    refatoring

--- packages/client/src/pages/Discovery/index.tsx
+++ packages/client/src/pages/Discovery/index.tsx
@@ -1,5 +1,5 @@
 import { Button, Link } from '@mui/material'
-import clsx from 'clsx'
+import { clsx } from 'clsx'
 import styles from './styles.module.scss'
 
 const Discovery = () => {
@@ -8,7 +8,7 @@ const Discovery = () => {
       <img className={styles.background} src="/bgImage.png" alt="" />
       <div className={clsx('container', styles.container)}>
         <div className={styles.wrapper}>
-          <h1 className={styles.title}>Bugs Survivors2</h1>
+          <h1 className={styles.title}>Bugs Survivors</h1>
           <h4 className={styles.title}>By StathamGames</h4>
           <Button variant="contained">Play game</Button>
           <ul className={styles.links}>
