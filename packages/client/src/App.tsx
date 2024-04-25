4a227c4f65a8b66efa1610776039385200a80a3e
DShulga92@gmail.com <Dshulga92@gmail.com>
2024-04-26 01:02:45 +0400

    добавил createTheme, ThemeProvider

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,64 @@
 import { useEffect } from 'react'
+import RegisterPage from './pages/registration/RegisterPage'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import PressStart from '../src/assets/fonts/PressStart2P-Regular.ttf'
 import './App.css'
 
 function App() {
+  const theme = createTheme({
+    typography: {
+      fontFamily: 'PressStart2P',
+    },
+    palette: {
+      primary: {
+        main: '#3F767B',
+      },
+      secondary: {
+        main: '#C1E0DF',
+        contrastText: 'black',
+      },
+    },
+    components: {
+      MuiButton: {
+        defaultProps: {
+          disableRipple: true,
+          disableElevation: true,
+        },
+
+        styleOverrides: {
+          root: {
+            padding: '8px 23px',
+            fontSize: '15px',
+            lineHeight: '26px',
+            color: '#F2F9F8',
+            font: 'Press Start 2P',
+            shadowBox: 'none',
+          },
+        },
+      },
+      MuiInputBase: {
+        styleOverrides: {
+          root: {
+            border: '1px solid #C1E0DF',
+            fontSize: '16px',
+            lineHeight: '24px',
+            fontFamily: 'Roboto',
+            fontWeight: '400',
+            backgroundColor: '#FFFFFF',
+            color: '#00000099',
+            marginBottom: '24px',
+          },
+        },
+      },
+      MuiTypography: {
+        styleOverrides: {
+          root: {
+            marginBottom: '24px',
+          },
+        },
+      },
+    },
+  })
   useEffect(() => {
     const fetchServerData = async () => {
       const url = `http://localhost:${__SERVER_PORT__}`
@@ -12,7 +69,13 @@ function App() {
 
     fetchServerData()
   }, [])
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <RegisterPage />
+      </ThemeProvider>
+    </div>
+  )
 }
 
 export default App
