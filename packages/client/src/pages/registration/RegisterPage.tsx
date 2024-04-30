2947494447d4b0b16e7976bb9635f5d0d97f629f
DShulga92@gmail.com <Dshulga92@gmail.com>
2024-04-30 21:32:34 +0400

    отредактировал верстку, добавил типы

--- packages/client/src/pages/registration/RegisterPage.tsx
+++ packages/client/src/pages/registration/RegisterPage.tsx
@@ -1,12 +1,10 @@
 import { Button, Grid, TextField, Typography } from '@mui/material'
 import style from './styles.module.scss'
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 
 const BASE_URL = 'https://ya-praktikum.tech/api/v2'
 
 function RegisterPage() {
-  const navigate = useNavigate()
   const initialData = {
     first_name: '',
     second_name: '',
@@ -16,30 +14,30 @@ function RegisterPage() {
     password: '',
   }
   const [dataForm, setDataForm] = useState(initialData)
-  const handleChange = (e: any) => {
-    switch (e.target.name) {
+  const handleChange = (event: React.ChangeEvent<HTMLFormElement>) => {
+    switch (event.target.name) {
       case 'first_name':
-        setDataForm({ ...dataForm, first_name: e.target.value })
+        setDataForm({ ...dataForm, first_name: event.target.value })
         break
       case 'second_name':
-        setDataForm({ ...dataForm, second_name: e.target.value })
+        setDataForm({ ...dataForm, second_name: event.target.value })
         break
       case 'email':
-        setDataForm({ ...dataForm, email: e.target.value })
+        setDataForm({ ...dataForm, email: event.target.value })
         break
       case 'phone':
-        setDataForm({ ...dataForm, phone: e.target.value })
+        setDataForm({ ...dataForm, phone: event.target.value })
         break
       case 'login':
-        setDataForm({ ...dataForm, login: e.target.value })
+        setDataForm({ ...dataForm, login: event.target.value })
         break
       case 'password':
-        setDataForm({ ...dataForm, password: e.target.value })
+        setDataForm({ ...dataForm, password: event.target.value })
         break
     }
   }
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     fetch(`${BASE_URL}/auth/signup`, {
       method: 'POST',
@@ -56,7 +54,7 @@ function RegisterPage() {
       })
       .then(() => {
         setDataForm(initialData)
-        navigate('/login')
+        // navigate('/login')
       })
       .catch(error => {
         console.log(error)
@@ -64,22 +62,26 @@ function RegisterPage() {
   }
 
   return (
-    <Grid container justifyContent="right" className={style.registration}>
+    <Grid container justifyContent={'right'} className={style.registration}>
       <Grid
-        container
         item
         xs={4}
+        height={'100vh'}
         sx={{
-          backgroundColor: '#2F464AB2',
-          alignItems: 'center',
+          display: 'flex',
           justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#2F464AB2',
         }}>
         <Grid
-          container
+          item
           xs={6}
-          direction={'column'}
-          justifyContent={'space-between'}
-          sx={{ height: '74vh' }}>
+          sx={{
+            height: '74%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}>
           <Grid
             item
             sx={{
@@ -91,51 +93,57 @@ function RegisterPage() {
             <Typography variant="h1" component="h1">
               BUGS
             </Typography>
-            <Typography variant="h2" component="h2">
+            <Typography variant="h2" component="h1">
               SURVIVORS
             </Typography>
           </Grid>
-          <form onChange={handleChange} onSubmit={handleSubmit}>
-            <Grid container gap="1.5rem">
-              <Typography variant="h5">Sign Up</Typography>
-              <TextField
-                value={dataForm.first_name}
-                name="first_name"
-                label="First name"></TextField>
+          <Grid item>
+            <form onSubmit={handleSubmit} onChange={handleChange}>
+              <Grid container gap="1.5rem">
+                <Typography variant="h5">Sign Up</Typography>
+                <TextField
+                  value={dataForm.first_name}
+                  name="first_name"
+                  label="First name"></TextField>
 
-              <TextField
-                value={dataForm.second_name}
-                name="second_name"
-                label="Second name"></TextField>
-              <TextField
-                value={dataForm.email}
-                name="email"
-                label="E-mail"></TextField>
-              <TextField
-                value={dataForm.phone}
-                name="phone"
-                label="Phone"></TextField>
-              <TextField
-                value={dataForm.login}
-                name="login"
-                label="Login"></TextField>
-              <TextField
-                value={dataForm.password}
-                name="password"
-                label="Password"></TextField>
-              <Grid container justifyContent={'space-between'}>
-                <Button
-                  type="button"
-                  onClick={() => navigate('/login')}
-                  color="secondary">
-                  SIGN IN
-                </Button>
-                <Button type="submit" color="primary">
-                  SIGN UP
-                </Button>
+                <TextField
+                  value={dataForm.second_name}
+                  name="second_name"
+                  label="Second name"></TextField>
+                <TextField
+                  value={dataForm.email}
+                  name="email"
+                  label="E-mail"></TextField>
+                <TextField
+                  value={dataForm.phone}
+                  name="phone"
+                  label="Phone"></TextField>
+                <TextField
+                  value={dataForm.login}
+                  name="login"
+                  label="Login"></TextField>
+                <TextField
+                  value={dataForm.password}
+                  name="password"
+                  label="Password"></TextField>
+                <Grid
+                  item
+                  display={'flex'}
+                  width={'100%'}
+                  justifyContent={'space-between'}>
+                  <Button
+                    type="button"
+                    onClick={() => console.log('to login')}
+                    color="secondary">
+                    SIGN IN
+                  </Button>
+                  <Button type="submit" color="primary">
+                    SIGN UP
+                  </Button>
+                </Grid>
               </Grid>
-            </Grid>
-          </form>
+            </form>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
