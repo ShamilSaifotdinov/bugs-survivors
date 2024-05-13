96ad7b649c948a71aed13fc71d1f867b306dff00
DShulga92@gmail.com <Dshulga92@gmail.com>
2024-05-13 12:24:52 +0400

    refactoring

--- packages/client/src/pages/Error500/index.tsx
+++ packages/client/src/pages/Error500/index.tsx
@@ -1,19 +1,12 @@
 import styles from './styles.module.scss'
 import { Button } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
 
 function ErrorPage500() {
-  const navigate = useNavigate()
   return (
     <div className={styles.container}>
-      <img
-        className={styles.img}
-        src="images/500.png"
-        alt="page500 background"
-      />
       <Button
         type="button"
-        onClick={() => navigate('/')}
+        href="/"
         sx={{ marginBottom: '16vh', position: 'absolute' }}
         variant="contained">
         BACK TO MAIN MENU
