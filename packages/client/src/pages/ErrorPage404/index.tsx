91b9ef3af7862cc6eda294d6a0de813eb01f8423
DShulga92@gmail.com <Dshulga92@gmail.com>
2024-05-13 12:27:58 +0400

    refactoring

--- packages/client/src/pages/ErrorPage404/index.tsx
+++ packages/client/src/pages/ErrorPage404/index.tsx
@@ -1,18 +1,14 @@
 import { Button } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
 import styles from './styles.module.scss'
 
 function ErrorPage404() {
-  const navigate = useNavigate()
   return (
     <div className={styles.container}>
-      <img src="/images/404.png" alt="page background" />
       <Button
         type="button"
-        onClick={() => navigate('/')}
+        href="/"
         sx={{ marginBottom: '16vh', position: 'absolute' }}
-        variant="contained"
-        className={styles.img}>
+        variant="contained">
         BACK TO MAIN MENU
       </Button>
     </div>
