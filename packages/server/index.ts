7df539a43e8551141dbf54b6033b9e5a6dfe6b7c
etacarinae333 <74928543+etacarinae333@users.noreply.github.com>
2024-06-14 15:04:28 +0200

    STA-71: Setup docker (Postgresql, pgadmin, server,client) (#46)
    * STA-71: Setup docker (Postgresql, pgadmin, server,client)
    
    * refactoring
    
    * refactoring
    
    * refactoring

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,18 +1,28 @@
 import dotenv from 'dotenv'
 import cors from 'cors'
+import express from 'express'
 dotenv.config()
 
-import express from 'express'
-import { createClientAndConnect } from './db'
+import { connectPostgres } from './postgres'
 
 const app = express()
 app.use(cors())
 const port = Number(process.env.SERVER_PORT) || 3001
 
-createClientAndConnect()
-
 app.get('/', (_, res) => {
   res.json('👋 Howdy from the server :)')
+})
+
+async function getPostgres() {
+  const db = await connectPostgres()
+  const time = await db?.query('SELECT NOW()')
+  return time
+}
+
+app.get('/testUsers', async (_, res) => {
+  const postgresData = await getPostgres()
+
+  res.json({ postgresData })
 })
 
 app.listen(port, () => {
