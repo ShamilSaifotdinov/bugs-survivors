8809fd893bb4a173c2ad68f94aafbae26ee5238d
DShulga92@gmail.com <Dshulga92@gmail.com>
2024-06-18 22:16:48 +0400

    add className in EmojiButton

--- packages/client/src/pages/Forum/components/PickEmoji/EmojiButton/index.tsx
+++ packages/client/src/pages/Forum/components/PickEmoji/EmojiButton/index.tsx
@@ -3,12 +3,19 @@ import styles from './styles.module.scss'
 import { IconButton } from '@mui/material'
 import EmojiPicker from '@emoji-mart/react'
 import data from '@emoji-mart/data'
+import clsx, { ClassValue } from 'clsx'
 
 type EmojiButtonProps = {
   onEmojiSelect: (e: any) => void
+  classNameBtn?: ClassValue
+  classNamePicker?: ClassValue
 }
 
-function EmojiButton({ onEmojiSelect }: EmojiButtonProps) {
+function EmojiButton({
+  onEmojiSelect,
+  classNameBtn,
+  classNamePicker,
+}: EmojiButtonProps) {
   const [isPickerVisible, setIsPickerVisible] = useState(false)
 
   const toggleEmojiBar = useCallback(
@@ -19,13 +26,13 @@ function EmojiButton({ onEmojiSelect }: EmojiButtonProps) {
   return (
     <div className={styles.container}>
       <IconButton
-        className={styles.emojiButton}
+        className={clsx(styles.emojiButton, classNameBtn)}
         onClick={toggleEmojiBar}
         sx={{ padding: 0 }}>
         <div className={styles.emojiButton} />
       </IconButton>
       {isPickerVisible ? (
-        <div className={styles.pickerWrapper}>
+        <div className={clsx(styles.pickerWrapper, classNamePicker)}>
           <EmojiPicker
             dynamicWidth={true}
             data={data}
