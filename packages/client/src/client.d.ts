95149c25316584583b899ceb3043f6ea5e02586f
ShamilSaifotdinov <shamil.saifotdinov@gmail.com>
2024-07-09 01:43:03 +0500

    Update client

--- packages/client/src/client.d.ts
+++ packages/client/src/client.d.ts
@@ -1,5 +1,6 @@
 declare const __EXTERNAL_SERVER_URL__: string
 declare const __INTERNAL_SERVER_URL__: string
+declare const __NODE_ENV__: string
 declare global {
   interface Window {
     APP_INITIAL_STATE: RootState
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   define: {
     __EXTERNAL_SERVER_URL__: JSON.stringify(process.env.EXTERNAL_SERVER_URL),
     __INTERNAL_SERVER_URL__: JSON.stringify(process.env.INTERNAL_SERVER_URL),
+    __NODE_ENV__: JSON.stringify(process.env.NODE_ENV),
   },
   plugins: [
     react(),
