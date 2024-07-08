00661395e8c65f50091e06f443fb275cf226b6cd
ShamilSaifotdinov <shamil.saifotdinov@gmail.com>
2024-07-08 13:18:41 +0500

    return client proxy

--- packages/client/server/index.ts
+++ packages/client/server/index.ts
@@ -8,6 +8,7 @@ import path from 'path'
 import { HelmetData } from 'react-helmet'
 import serialize from 'serialize-javascript'
 import cookieParser from 'cookie-parser'
+import { createProxyMiddleware } from 'http-proxy-middleware'
 
 const port = process.env.CLIENT_PORT || 3000
 const clientPath = path.join(__dirname, '..')
@@ -31,6 +32,15 @@ async function createServer() {
     app.use(
       express.static(path.join(clientPath, 'dist/client'), { index: false })
     )
+  }
+
+  if (isDev) {
+    const backendProxy = createProxyMiddleware({
+      target: process.env.EXTERNAL_SERVER_URL,
+      changeOrigin: true,
+    })
+
+    app.all('/api/v2/*', backendProxy)
   }
 
   app.get('*', async (req, res, next) => {
